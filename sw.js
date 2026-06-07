e4ed8b6bbfdbde6fb563472375abee3cb58525d5
thesurfingdiver <aarondoyler@gmail.com>
2026-06-07 10:45:54 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'medrx-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install the service worker and cache the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return the cached version if found, otherwise fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
